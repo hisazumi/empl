126314acc621fc782b73c60a129eb4a992a6a768
Kenji Hisazumi <kenji@hisazumi.jp>
2017-02-18 15:07:20 +0900

    added a testcase for including c-language

---
+++ test/c01.c
@@ -0,0 +1,20 @@
+define ULDATA {
+    uint32 dest;
+    uint32 src;
+    uint32 fid;
+    uint8 htl;
+    uint8 ackn;
+    uint8 retryn;
+    PACKET_TYPE pt;
+    uint1 loop_flag;
+    uint32 header_size;
+};
+
+int main() {
+    match(p) {
+        case ULDATA[ 32, 40 ] {
+        }
+        case ULDATA[ 30 ] {
+        }
+    }
+}
