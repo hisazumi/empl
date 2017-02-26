43107c90c7c20703af2c04801d4642e27fef2d21
Kenji Hisazumi <kenji@hisazumi.jp>
2017-02-26 10:01:40 +0900

    support typedef partially

--- cheader.py
+++ cheader.py
@@ -9,16 +9,24 @@ structm = metamodel_from_file('Cstruct.tx')
 # Main
 rest = open(sys.argv[1]).read()
 while True:
-    pos = rest.find('struct')
-    if pos < 0:
+    spos = rest.find('struct')
+    tpos = rest.find('typedef')
+    if spos < 0 or tpos < 0:
         break
+    elif spos > 0 and (spos < tpos or tpos < 0):
+        pos = spos
+    elif tpos > 0 and (tpos < spos or spos < 0):
+        pos = tpos
     
     try:
         m = structm.model_from_str(rest[pos:])
-        print('read: ' + m.struct.name)
+        if m.struct:
+            print('read: ' + m.struct.name)
+        else:
+            print('read: ' + m.typedef.name)
         rest = '\n'.join(m.rest)
     except TextXSyntaxError as e:
-        print('warn: ', rest[pos:pos+40])
+        print('warn: ', rest[pos:pos+30])
         #print(e)
         rest = rest[pos + 7:]
     
